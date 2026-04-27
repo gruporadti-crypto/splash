fd5f61c86d90a3b11f5b88be5e4b72b96b9f6c94
gruporadti-crypto <gruporadti@gmail.com>
2026-04-27 17:36:47 -0300

    Update Supabase connection settings

--- api/agenda.php
+++ api/agenda.php
@@ -1,6 +1,6 @@
 <?php
 // --- CONFIGURAÇÕES DE CONEXÃO SUPABASE ---
-$host     = 'db.xzemserhahccodubenfj.supabase.co';
+$host     = 'aws-0-sa-east-1.pooler.supabase.co';
 $port     = '6543'; 
 $dbname   = 'postgres';
 $user     = 'postgres';
